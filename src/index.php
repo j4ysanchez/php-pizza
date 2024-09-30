139888f4fdabf388dc8a5c9da595574155db0c32
Jason Sanchez <13209003+j4ysanchez@users.noreply.github.com>
2024-09-29 19:41:29 -0600

    Initial commit Added additional toppings and new extra large size

--- src/index.php
+++ src/index.php
@@ -17,15 +17,25 @@
             <option value="small">Small</option>
             <option value="medium">Medium</option>
             <option value="large">Large</option>
+            <option value="xlarge">Extra Large</option>
         </select><br><br>
 
         <label for="toppings">Toppings:</label><br>
+        
         <input type="checkbox" id="pepperoni" name="toppings[]" value="Pepperoni">
         <label for="pepperoni">Pepperoni</label><br>
+        
         <input type="checkbox" id="mushrooms" name="toppings[]" value="Mushrooms">
         <label for="mushrooms">Mushrooms</label><br>
+        
         <input type="checkbox" id="onions" name="toppings[]" value="Onions">
-        <label for="onions">Onions</label><br><br>
+        <label for="onions">Onions</label><br>
+
+        <input type="checkbox" id="extra_cheese" name="toppings[]" value="Extra Cheese">
+        <label for="extra_cheese">Extra Cheese</label><br>
+
+        <input type="checkbox" id="sausage" name="toppings[]" value="Sausage">
+        <label for="sausage">Sausage</label><br>
 
         <input type="submit" value="Order Pizza">
     </form>
